75ff81b606b59020db90e778617f013b81ac3d19
manjaka13 <manjaka.rajaonson@gmail.com>
2023-09-28 11:20:42 +0300

    add dashboard nav

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,11 +1,47 @@
-import React, { FC } from "react";
+import React, { FC, useContext } from "react";
+import { UserContext } from "src/hooks/useUser";
+import Button from "src/components/Button";
+import Logo from "src/images/logo.png";
 
 /**
  * Main dashboard
  */
 
 const Dashboard: FC = (): JSX.Element => {
-	return <div>Your dashboard</div>;
+	const { user, signOut } = useContext(UserContext);
+
+	console.log(user);
+
+	return (
+		<div className="dashboard">
+			<nav className="dashboard__nav">
+				<div className="head">
+					<img className="icon" src={Logo} alt="Logo" />
+					<p className="date">26 / 09 / 2023</p>
+				</div>
+				<form className="search">
+					<input className="input" placeholder="Rechercher" required />
+					<Button>Rechercher</Button>
+				</form>
+				<ul className="dashboard__actions">
+					<li className="item" title="Ajout note à faire">
+						Ajouter à faire
+					</li>
+					<li className="item" title="Ajout revenu/dépense">
+						Ajouter un flux
+					</li>
+					<li className="item" onClick={signOut} title="Vous déconnecter">
+						<img
+							className="icon avatar"
+							src={user?.photoURL || ""}
+							alt="User avatar"
+						/>{" "}
+						Se déconnecter
+					</li>
+				</ul>
+			</nav>
+		</div>
+	);
 };
 
 export default Dashboard;
